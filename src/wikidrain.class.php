337d87bbc4d454e05266dd84b9744f94d8c1293f
abreksa4 <abreksa4@gmail.com>
2013-12-19 01:08:16 -0800

    added wikidrain class

---
+++ src/wikidrain.class.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 12/19/13
+ * Time: 1:07 AM
+ */
+
+class wikidrain {
+
+    protected $_apiUrl;
+    protected $_wikiQuery;
+    protected $_wikiPage;
+    protected $_wikiData;
+
+    public function __construct($lang){
+        $this->setLang($lang);
+    }
+
+    public function setLang($lang){
+        $this->_apiUrl = "http://{$lang}.wikipedia.org/w/api.php";
+    }
+
+    public function getApi(){
+        return $this->_apiUrl;
+    }
+
+    public function setQuery($query){
+        $this->_wikiQuery = $query;
+    }
+
+    public function getQuery(){
+        return $this->_wikiQuery;
+    }
+
+    public function queryClean(){
+        $this->_wikiQuery = htmlspecialchars($this->_wikiQuery);
+    }
+
+    public function getSections(){
+        $this->_wikiData = "{$this->_apiUrl}?format=html&action=parse&prop=sections&title={$this->_wikiPage}";
+        return $this->_wikiData;
+    }
+}
