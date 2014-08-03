2b0c6286d6d37d7999626ca05793075e414b7cb7
Andrew Breksa <abreksa4@gmail.com>
2014-08-03 01:36:22 -0700

    Cleaned up

--- api.php
+++ api.php
@@ -2,12 +2,13 @@
 while (isset($_GET['action']) & isset($_GET['query']) & isset($_GET['useragent'])) {
     $action = $_GET['action'];
     $query = $_GET['query'];
+    $useragent = $_GET['useragent'];
     if (isset($_GET['numresults'])) {
         $numresults = $_GET['numresults'];
     } else {
         $numresults = 10;
     }
-    $wikidrain = new wikidrain($_GET['useragent']);
+    $wikidrain = new wikidrain($useragent);
     switch ($action) {
         case('search'):
             $results = $wikidrain->Search($query, $numresults);
